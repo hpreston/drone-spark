671b408667525143f4395179e643b1c320b4abce
Hank Preston <hank.preston@gmail.com>
2016-07-24 19:27:30 -0400

    Change to get the ENVs from the drone input data rather than environment variables.  And add some debug info...

--- send_message.py
+++ send_message.py
@@ -60,34 +60,58 @@ def verify_roomId(roomId):
     else:
         return False
 
-def load_drone_env_variables():
+def load_drone_env_variables(payload):
     '''
     Bring in standard environment variables passed by Drone
     '''
 
     global drone_env
 
-    drone_env["DRONE"] = os.getenv("DRONE")
-    drone_env["DRONE_REPO"] = os.getenv("DRONE_REPO")
-    drone_env["DRONE_BRANCH"] = os.getenv("DRONE_BRANCH")
-    drone_env["DRONE_COMMIT"] = os.getenv("DRONE_COMMIT")
-    drone_env["DRONE_DIR"] = os.getenv("DRONE_DIR")
-    drone_env["DRONE_BUILD_NUMBER"] = os.getenv("DRONE_BUILD_NUMBER")
-    drone_env["DRONE_PULL_REQUEST"] = os.getenv("DRONE_PULL_REQUEST")
-    drone_env["DRONE_JOB_NUMBER"] = os.getenv("DRONE_JOB_NUMBER")
-    drone_env["DRONE_TAG"] = os.getenv("DRONE_TAG")
+    # Fron container environment variables...
+    # drone_env["DRONE"] = os.getenv("DRONE")
+    # drone_env["DRONE_REPO"] = os.getenv("DRONE_REPO")
+    # drone_env["DRONE_BRANCH"] = os.getenv("DRONE_BRANCH")
+    # drone_env["DRONE_COMMIT"] = os.getenv("DRONE_COMMIT")
+    # drone_env["DRONE_DIR"] = os.getenv("DRONE_DIR")
+    # drone_env["DRONE_BUILD_NUMBER"] = os.getenv("DRONE_BUILD_NUMBER")
+    # drone_env["DRONE_PULL_REQUEST"] = os.getenv("DRONE_PULL_REQUEST")
+    # drone_env["DRONE_JOB_NUMBER"] = os.getenv("DRONE_JOB_NUMBER")
+    # drone_env["DRONE_TAG"] = os.getenv("DRONE_TAG")
+    #
+    # drone_env["CI"] = os.getenv("CI")
+    # drone_env["CI_NAME"] = os.getenv("CI_NAME")
+    # drone_env["CI_REPO"] = os.getenv("CI_REPO")
+    # drone_env["CI_BRANCH"] = os.getenv("CI_BRANCH")
+    # drone_env["CI_COMMIT"] = os.getenv("CI_COMMIT")
+    # drone_env["CI_BUILD_NUMBER"] = os.getenv("CI_BUILD_NUMBER")
+    # drone_env["CI_PULL_REQUEST"] = os.getenv("CI_PULL_REQUEST")
+    # drone_env["CI_JOB_NUMBER"] = os.getenv("CI_JOB_NUMBER")
+    # drone_env["CI_BUILD_DIR"] = os.getenv("CI_BUILD_DIR")
+    # drone_env["CI_BUILD_URL"] = os.getenv("CI_BUILD_URL")
+    # drone_env["CI_TAG"] = os.getenv("CI_TAG")
 
-    drone_env["CI"] = os.getenv("CI")
-    drone_env["CI_NAME"] = os.getenv("CI_NAME")
-    drone_env["CI_REPO"] = os.getenv("CI_REPO")
-    drone_env["CI_BRANCH"] = os.getenv("CI_BRANCH")
-    drone_env["CI_COMMIT"] = os.getenv("CI_COMMIT")
-    drone_env["CI_BUILD_NUMBER"] = os.getenv("CI_BUILD_NUMBER")
-    drone_env["CI_PULL_REQUEST"] = os.getenv("CI_PULL_REQUEST")
-    drone_env["CI_JOB_NUMBER"] = os.getenv("CI_JOB_NUMBER")
-    drone_env["CI_BUILD_DIR"] = os.getenv("CI_BUILD_DIR")
-    drone_env["CI_BUILD_URL"] = os.getenv("CI_BUILD_URL")
-    drone_env["CI_TAG"] = os.getenv("CI_TAG")
+    # From Payload
+    drone_env["DRONE"] = True
+    drone_env["DRONE_REPO"] = payload["repo"]["full_name"]
+    drone_env["DRONE_BRANCH"] = payload["build"]["branch"]
+    drone_env["DRONE_COMMIT"] = payload["build"]["commit"]
+    drone_env["DRONE_DIR"] = payload["workspace"]["path"]
+    drone_env["DRONE_BUILD_NUMBER"] = payload["build"]["number"]
+    drone_env["DRONE_PULL_REQUEST"] = ""
+    drone_env["DRONE_JOB_NUMBER"] = payload["build"]["number"]
+    drone_env["DRONE_TAG"] = payload["build"]["tag"]
+
+    drone_env["CI"] = True
+    drone_env["CI_NAME"] = "drone"
+    drone_env["CI_REPO"] = payload["repo"]["full_name"]
+    drone_env["CI_BRANCH"] = payload["build"]["branch"]
+    drone_env["CI_COMMIT"] = payload["build"]["commit"]
+    drone_env["CI_BUILD_NUMBER"] = payload["build"]["number"]
+    drone_env["CI_PULL_REQUEST"] = ""
+    drone_env["CI_JOB_NUMBER"] = payload["build"]["number"]
+    drone_env["CI_BUILD_DIR"] = payload["workspace"]["path"]
+    drone_env["CI_BUILD_URL"] = payload["repo"]["link_url"]
+    drone_env["CI_TAG"] = payload["build"]["tag"]
 
 def message_var_exchange(message):
     '''
@@ -100,22 +124,34 @@ def message_var_exchange(message):
 
     return message
 
+def debug_actions(roomId, message):
+    '''
+    For testing
+    '''
+
+    spark_message = {
+        "roomId": roomId,
+        "text": "Debug: \n \n " + message
+    }
+
+    response = requests.post(
+        spark_urls["messages"],
+        headers=spark_headers,
+        json=spark_message)
+
 def main():
     payload = drone.plugin.get_input()
     vargs = payload["vargs"]
 
-    print("Drone Payload: ")
-    print(payload)
-    print(" ")
-
     # Pull in Drone Environment Variables
-    load_drone_env_variables()
+    load_drone_env_variables(payload)
     # Replace any $VAR references in the message
     vargs["message"] = message_var_exchange(vargs["message"])
 
     # Prepare headers and message objects
     spark_headers["Authorization"] = "Bearer %s" % (vargs["auth_token"])
     spark_message = {}
+
 
     # Determine destination for message
     try:
@@ -128,6 +164,9 @@ def main():
             spark_message["toPersonEmail"] = vargs["personEmail"]
         else:
             raise(LookupError("Requires valid roomId, roomName, or personEmail to be provided.  "))
+
+    # Debug payload
+    debug_actions(roomId, str(payload))
 
     spark_message["markdown"] = vargs["message"]
     # print(spark_message)
